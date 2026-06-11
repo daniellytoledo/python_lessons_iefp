a04d7db14594c80a228cce091b81b31be68b3b3b
Dani <daniellytoledosouto@gmail.com>
2026-06-11 12:37:00 +0100

    exerc ciclos

---
+++ exercicios/ex06.py
@@ -0,0 +1,28 @@
+import time
+
+print("\n")
+print("::::::::::::::::::::::::::::::::::::::::::::::::::")
+print("                    CICLOS")
+print("::::::::::::::::::::::::::::::::::::::::::::::::::")
+print("\n")
+
+print(":::::::::::: números de 1 a 10 ::::::::::::")
+
+# RANGE
+for i in range(1,6):
+    print(i, "💣")
+    time.sleep(0.5)
+print("💥💥💥💥💥")
+print("💥💥☠️💥💥")
+print("💥💥💥💥💥")
+
+print("\n")
+
+frutas = ["maçã", "banana", "laranja"]
+for fruta in frutas:
+    print("Eu gosto de ", fruta)
+
+print("\n")
+
+for letra in "danielly":
+    print(letra)
