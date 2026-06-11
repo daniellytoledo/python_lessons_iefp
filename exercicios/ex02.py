b8a4bef583c483a55793c52fb70124e4a05b8341
Dani <daniellytoledosouto@gmail.com>
2026-06-11 09:39:38 +0100

    exercicio

---
+++ exercicios/ex02.py
@@ -0,0 +1,7 @@
+print("::::::::::::::::::::::::::::::::::::::::::::::::::")
+print("RECEBER DADOS DO UTILIZADOR")
+print("::::::::::::::::::::::::::::::::::::::::::::::::::")
+print("\n")
+
+idade        = int(input("Qual a sua idade?"))
+atura_string = input("Qual a sua altura em metros?")
