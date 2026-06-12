edfea9131cdadb4a5fee39a4e9eaba98d51354b8
Dani <daniellytoledosouto@gmail.com>
2026-06-12 12:14:09 +0100

    - def

--- exercicios/ex11-try.py
+++ exercicios/ex11-try.py
@@ -8,22 +8,12 @@ print("  SOLICITAR UMA NOTA NO INTERVALO DE 0 A 20 ")
 print(":"*49)
 print("\n")
 
-def lancarNota():
-    while True:
-        try:
-            nota = float(input("Digite um valor entre 0 a 20: ").replace(",", "."))
-            if 0 <= nota <= 20:
-                break
-            else:
-                print("A nota tem que estar entre 0 e 20")
-        except ValueError:
-            print("ERRO: Digite um número válido.")
-
-    while True:
-        r = input("Deseja adicionar mais uma nota? [S/N]").upper()
-        if r == "S":
-         lancarNota()
-        elif r == "N":
-         break
+while True:
+    try:
+        nota = float(input("Digite um valor entre 0 a 20: ").replace(",", "."))
+        if 0 <= nota <= 20:
+            break
         else:
-            print("Responda apenas 'S' ou 'N'.")
+            print("A nota tem que estar entre 0 e 20")
+    except ValueError:
+        print("ERRO: Digite um número válido.")
